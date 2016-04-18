7e8dd7a8fe112788b9f2dba38ef1bb3ad44bbb65
magndahl <magnus.dahl42@gmail.com>
2016-04-18 15:54:21 +0200

    First tries fitting

--- model_selection_ext_horizon.py
+++ model_selection_ext_horizon.py
@@ -15,6 +15,7 @@ from statsmodels.sandbox.regression.predstd import wls_prediction_std
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from model_selection import mlin_regression, summary_to_file, linear_map, rmse, mae, mape
 
 import pandas as pd
 
@@ -31,7 +32,7 @@ def gen_fit_df(ts_start, ts_end, varnames, timeshifts, pointcode=71699):
     df['prod'] = sq.fetch_production(ts_start, ts_end)
     for timeshift in timeshifts:
         
-        df['prod%ibefore'%timeshift] = sq.fetch_production(h_hoursbefore(ts_start, timeshift),\
+        df['prod%ihbefore'%timeshift] = sq.fetch_production(h_hoursbefore(ts_start, timeshift),\
                                                           h_hoursbefore(ts_end, timeshift))
         for v in varnames:
             ens_mean = ens.load_ens_mean_avail_at10_series(v, ts_start, ts_end, pointcode=71699)
@@ -39,10 +40,26 @@ def gen_fit_df(ts_start, ts_end, varnames, timeshifts, pointcode=71699):
                                             h_hoursbefore(ts_start, timeshift),\
                                             h_hoursbefore(ts_end, timeshift),\
                                             pointcode=71699)
-            df['%s%idiff'%(v,timeshift)] = ens_mean - ens_mean_before
+            df['%s%ihdiff'%(v,timeshift)] = ens_mean - ens_mean_before
     
     
     return df        
         
         
-        
+all_data = gen_fit_df(dt.datetime(2016,1,26,1), dt.datetime(2016,4,1,0), ['Tout', 'vWind', 'hum', 'sunRad'], [48,168])
+y = all_data['prod']
+
+#%%
+ts = ens.gen_hourly_timesteps(dt.datetime(2016,1,26,1), dt.datetime(2016,4,1,0))
+X = all_data[['prod48hbefore', 'Tout48hdiff', 'sunRad48hdiff', 'prod168hbefore', 'Tout168hdiff', 'vWind168hdiff', 'sunRad168hdiff',  'hum168hdiff']]
+est = mlin_regression(y, X, add_const=False)
+
+fv = est.fittedvalues
+plt.close('all')
+sns.jointplot(fv, y)
+plt.figure()
+plt.plot_date(ts, y, 'b-')
+plt.plot_date(ts, fv, 'r-')
+
+err = y-fv
+print rmse(err), mae(err), mape(err,y)
