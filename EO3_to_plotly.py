28428676c75ca27751e31891c7a1eca2deb0d1bc
Magnus Dahl <magnus@fanta.auitdrift.client.au.dk>
2016-06-09 11:51:25 +0200

    Time horizon changed to sync up and legends added

--- EO3_to_plotly.py
+++ EO3_to_plotly.py
@@ -9,6 +9,7 @@ matplotlib.use("Agg") # this is needed to rund under cron
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import AnchoredText
 import plotly.plotly as ply
+import plotly.tools as tls
 import plotly.graph_objs as go
 from error_tools import rmse, mae, mape
 
@@ -28,45 +29,48 @@ def main(argv):
             print "Argument must be 'online' or a valid date in yyyy-mm-dd format."
             sys.exit()
 
-
-    # Plot prediction for tomorrow
-    ts_start = dt.datetime(today.year, today.month, today.day, hour=1)\
-            +dt.timedelta(hours=24)
-    ts_end = ts_start + dt.timedelta(hours=23)
-
-    predicted = sq.fetch_EO3_10oclock_forecast(ts_start, ts_end)
-
-    fig = plt.figure()
-    plt.plot_date(ens.gen_hourly_timesteps(ts_start, ts_end), predicted,\
-            'r-',label='Forecast')
-    plt.ylabel("Production [MW]")
-    plt.title("Tomorrow: " + str(today+dt.timedelta(days=1)))
-    layout = go.Layout(showlegend=True)
-    ply.plot_mpl(fig, filename='EO3 forecast for tomorrow', auto_open=False)
-
     # Plot prediction for yesterday + benchmark
-
     ts_start_yest = dt.datetime(today.year, today.month, today.day, hour=1)\
             +dt.timedelta(hours=-24)
     ts_end_yest = ts_start_yest + dt.timedelta(hours=23)
 
-    predicted_yest = sq.fetch_EO3_10oclock_forecast(ts_start_yest, ts_end_yest)
+    predicted_yest = sq.fetch_EO3_9oclock_forecast(ts_start_yest, ts_end_yest)
     actual_prod_yest = sq.fetch_production(ts_start_yest, ts_end_yest)
-
 
     fig_yest = plt.figure()
     plt.plot_date(ens.gen_hourly_timesteps(ts_start_yest, ts_end_yest),\
-            predicted_yest, 'r-', label='Forecast')
+            predicted_yest, 'r-', lw=2, label='Forecast')
     plt.plot_date(ens.gen_hourly_timesteps(ts_start_yest, ts_end_yest),\
-            actual_prod_yest, 'k-', label='Realized production')
+            actual_prod_yest, 'k-', lw=2, label='Realized production')
     plt.ylabel("Production [MW]")
     plt.title("Yesterday: " + str(today+dt.timedelta(days=-1)))
     err = predicted_yest-actual_prod_yest
     err_string = """RMSE = %2.2f MW\nMAE = %2.2f MW\nMAPE = %2.2f %%""" \
                 % (rmse(err), mae(err), 100*mape(err, actual_prod_yest))
     plt.annotate(err_string, xy=(0.80, 0.80), xycoords='axes fraction')
+    plotly_fig_yest = tls.mpl_to_plotly(fig_yest)
+    plotly_fig_yest['layout']['showlegend'] = True
+    plotly_fig_yest['layout']['legend'] = dict(x=0.75, y='0.05')
+    url_yest = ply.plot(plotly_fig_yest, filename="Yesterday's EO3 forecast", auto_open=False)
 
-    ply.plot_mpl(fig_yest, filename="Yesterday's EO3 forecast", auto_open=False)#, plot_options={'layout':layout})
+
+
+    # Plot prediction for tomorrow
+    ts_start = dt.datetime(today.year, today.month, today.day, hour=1)\
+            +dt.timedelta(hours=24)
+    ts_end = ts_start + dt.timedelta(hours=23)
+
+    predicted = sq.fetch_EO3_9oclock_forecast(ts_start, ts_end)
+
+    fig = plt.figure()
+    plt.plot_date(ens.gen_hourly_timesteps(ts_start, ts_end), predicted,\
+            'r-', lw=2, label='Forecast')
+    plt.ylabel("Production [MW]")
+    plt.title("Tomorrow: " + str(today+dt.timedelta(days=1)))
+    plotly_fig = tls.mpl_to_plotly(fig)
+    plotly_fig['layout']['showlegend'] = True
+    plotly_fig['layout']['legend'] = dict(x=0.75, y=0.05)
+    url = ply.plot(plotly_fig, filename='EO3 forecast for tomorrow', auto_open=False)
 
 
 if __name__ == "__main__":
