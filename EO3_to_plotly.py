f63f8ac23c94bbc6b9924b4a0afe99fe39b7c193
Magnus Dahl <magnus@fanta.auitdrift.client.au.dk>
2016-06-10 15:53:45 +0200

    Added historic plot

--- EO3_to_plotly.py
+++ EO3_to_plotly.py
@@ -29,6 +29,29 @@ def main(argv):
             print "Argument must be 'online' or a valid date in yyyy-mm-dd format."
             sys.exit()
 
+    # plot_historic_EO3_forecast and benchmark
+    fig = plt.figure()
+    ts_end = dt.datetime(today.year, today.month, today.day, 0)
+    ts = ens.gen_hourly_timesteps(dt.datetime(2016,2,26,1), ts_end)
+
+    predicted = sq.fetch_EO3_9oclock_forecast(ts[0], ts[-1])
+    prod = sq.load_local_production(ts[0], ts[-1])
+    plt.plot_date(ts, prod, 'k-', lw=2, label='Realized production')
+    plt.plot_date(ts, predicted, 'r-', lw=2, label='Forecast')
+    plt.ylabel("Production [MW]")
+    err = predicted - prod
+    err_string = """RMSE = %2.2f MW\nMAE = %2.2f MW\nMAPE = %2.2f %%""" \
+                % (rmse(err), mae(err), 100*mape(err, prod))
+    plt.annotate(err_string, xy=(0.80, 0.80), xycoords='axes fraction')
+
+    plotly_fig_hist = tls.mpl_to_plotly(fig)
+    plotly_fig_hist['layout']['showlegend'] = True
+    plotly_fig_hist['layout']['autosize'] = True
+    plotly_fig_hist['layout']['legend'] = dict(x=0.75, y='0.05')
+
+    ply.plot(plotly_fig_hist, filename="EO3 forecast history", auto_open=False)
+
+
     # Plot prediction for yesterday + benchmark
     ts_start_yest = dt.datetime(today.year, today.month, today.day, hour=1)\
             +dt.timedelta(hours=-24)
@@ -50,6 +73,7 @@ def main(argv):
     plt.annotate(err_string, xy=(0.80, 0.80), xycoords='axes fraction')
     plotly_fig_yest = tls.mpl_to_plotly(fig_yest)
     plotly_fig_yest['layout']['showlegend'] = True
+    plotly_fig_yest['layout']['autosize'] = True
     plotly_fig_yest['layout']['legend'] = dict(x=0.75, y='0.05')
     url_yest = ply.plot(plotly_fig_yest, filename="Yesterday's EO3 forecast", auto_open=False)
 
@@ -69,8 +93,10 @@ def main(argv):
     plt.title("Tomorrow: " + str(today+dt.timedelta(days=1)))
     plotly_fig = tls.mpl_to_plotly(fig)
     plotly_fig['layout']['showlegend'] = True
+    plotly_fig['layout']['autosize'] = True
     plotly_fig['layout']['legend'] = dict(x=0.75, y=0.05)
     url = ply.plot(plotly_fig, filename='EO3 forecast for tomorrow', auto_open=False)
+
 
 
 if __name__ == "__main__":
